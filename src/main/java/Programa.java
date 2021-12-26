46e7119046a5f5b6b2257cdc52efaf6079aea711
yurihallan <yurihallan9@gmail.com>
2021-12-26 13:01:27 -0400

    Crud na classe Programa.java Main feito com os metodos do hibernate.

--- src/main/java/Programa.java
+++ src/main/java/Programa.java
@@ -25,7 +25,10 @@ public class Programa {
 
         // criar instancias
         Telefones telefonesAdd = new Telefones("98555212","celular",92);
-        Usuario usuarioAdd = new Usuario("Pedro","pedro@gmail.com","1345545",telefonesAdd);
+        Usuario usuarioAdd = new Usuario("Jose","pedro@gmail.com","1345545",telefonesAdd);
+
+        Telefones telefonesAdd2 = new Telefones("98555212","Telefone",11);
+        Usuario usuarioAdd2 = new Usuario("Sara","Sara@gmail.com","08200121515",telefonesAdd2);
 
         // iniciar uma transação para adicionar
         entityManager.getTransaction().begin();
@@ -33,19 +36,47 @@ public class Programa {
         //persiste os dados
         entityManager.persist(telefonesAdd);
         entityManager.persist(usuarioAdd);
+        entityManager.persist(telefonesAdd2);
+        entityManager.persist(usuarioAdd2);
 
         // commit/ Finaliza uma transação, persistindo os dados
         entityManager.getTransaction().commit();
 
 
-
-        //consulta dos dados
-        Usuario findUsers = entityManager.find(Usuario.class,1);
+        //Trazendo somente 1 resultado
+        Usuario findUsers = entityManager.find(Usuario.class,2);
 
         System.out.println(findUsers);
 
 
-        //Fecha as conexão das entity
+        //Trazendo uma lista dos dados
+        String sqList = "SELECT * from Usuario";
+        List<Usuario> usuarioList = entityManager.createNativeQuery(sqList,Usuario.class).getResultList();
+        usuarioList.forEach(usuario -> System.out.println("Lista -> "+ usuario));
+
+
+
+        //Alterar os dados
+        entityManager.getTransaction().begin();
+//
+        findUsers.setNome("Fernanda");
+        findUsers.setEmail("Fernanda@gmail.com");
+
+        entityManager.getTransaction().commit();
+        System.out.println(findUsers);
+
+
+        //Remover um dado
+        entityManager.getTransaction().begin();
+
+        entityManager.remove(findUsers);
+
+        entityManager.getTransaction().commit();
+
+
+
+
+        //Encerrar o gerenciador de entidades e encerrar a fabrica de gerenciadores de entidade.
         entityManager.close();
         entityManagerFactory.close();
     }
